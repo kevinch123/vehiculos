6efbcfd61e6cd304f5e374f433a93d0f081afdd4
kevinchi <kevinchi78@gmail.com>
2024-05-17 08:59:44 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { VehiculosListarComponent } from './vehiculos/vehiculos-listar/vehiculos-listar.component';
+import { VehiculosCreateComponent } from './vehiculos/vehiculos-create/vehiculos-create.component';
 
 export const routes: Routes = [{
     path:'',
-        component:VehiculosListarComponent
+      component:VehiculosListarComponent
+    },
+    {
+      path:'newvehiculo',
+      component:VehiculosCreateComponent
+    },
 
-}];
+    {
+        path:'vehiculos',
+          component:VehiculosListarComponent
+        },
+
+  ];
+
+
